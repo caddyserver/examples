b9ebefd3c503fa788664fa973851397b16f22b98
Theofanis Despoudis <thdespou@hotmail.com>
2018-10-23 06:47:47 +0100

    Added Flask with FastCGI example (#135)

---
+++ flask-fcgi/README.md
@@ -0,0 +1,22 @@
+
+Run a Flask site on Caddy using FastCGI adapter
+---------------------------
+Flask ♥️ Caddy
+
+Example Caddy configuration file to get started with Caddy and Flask via the FastCGI directive.
+
+## How to run
+
+1. Install the requirements:
+
+```
+pip3 install -r requirements.txt
+```
+
+2. Run the minimal Flask server
+
+```
+python3 app.py
+```
+
+3. Navigate to localhost:9000 for a greeting!
---
+++ flask-fcgi/app.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import logging
+
+from flup.server.fcgi import WSGIServer
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+def main(app):
+    try:
+        WSGIServer(app, bindAddress='./hello-world.sock', umask=0000).run()
+    except (KeyboardInterrupt, SystemExit, SystemError):
+        logging.info("Shutdown requested...exiting")
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+
+
+if __name__ == '__main__':
+    main(app)
